2490d8e8584e33072b2107c850f708b2219f02b5
Savant-sys <86020928+Savant-sys@users.noreply.github.com>
2022-05-04 16:34:01 -0700

    Add files via upload

---
+++ CollegeTouring/list_of_campus.h
@@ -0,0 +1,27 @@
+#ifndef LIST_OF_CAMPUS_H
+#define LIST_OF_CAMPUS_H
+
+#include "campus.h"
+
+//namespace Ui
+//{
+//    class listOfCampuses;
+//}
+
+
+///*!
+// * \brief List of Campus class
+// * Serves as a UI for the popup campus list, with initial distance to Saddleback
+// */
+//class listOfCampus : public QDialog
+//{
+//    Q_OBJECT
+//public:
+//    explicit listOfCampus(vector<Campus> campuses, QWidget *parent = nullptr);
+//    ~listOfCampus();
+
+//private:
+//    Ui::listOfCampuses *ui; /*!< UI of popup window */
+//};
+
+#endif // LIST_OF_CAMPUS_H
---
+++ CollegeTouring/menuwidget.h
@@ -0,0 +1,54 @@
+#ifndef MENUWIDGET_H
+#define MENUWIDGET_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+#include <QDebug>
+#include <vector>
+
+#include "mainwindow.h"
+
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QComboBox>
+#include <QPushButton>
+
+#include "campus.h"
+
+/*!
+ * \brief Widget class used for editing restaurant menus. Inherits from QWidget.
+ */
+
+class MenuWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    //! Constructor
+    //! \brief Initializes all ui elements with restaurant passed into the constructor.
+    explicit MenuWidget(Campus campus, MainWindow *parent = nullptr);
+
+private:
+    QHBoxLayout *layout; /*!< Layout to hold all ui elements */
+    QLabel *nameLabel; /*!< Restaurant name */
+    QComboBox *menuItems; /*!< Combobox of menu items */
+    QPushButton *addNewItemButton; /*!< Button to add a new item */
+    QPushButton *deleteItemButton; /*!< Button to delete an item */
+    QPushButton *editItemButton; /*!< Button to edit an item */
+    MainWindow *parent; /*!< Main application reference */
+
+    Campus campus; /*!< Restaurant associated with the widget */
+
+private slots:
+
+    void on_AddMenuItemClicked();
+
+    void on_DeleteMenuItemClicked();
+
+    void on_EditMenuItemClicked();
+
+signals:
+
+};
+
+#endif // MENUWIDGET_H
