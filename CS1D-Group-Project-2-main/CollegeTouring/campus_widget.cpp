6f332df630da5068e0cb08da28f6417442507062
42-Max-42 <91645022+42-Max-42@users.noreply.github.com>
2022-05-17 00:52:38 -0700

    Add files via upload

---
+++ CS1D-Group-Project-2-main/CollegeTouring/campus_widget.cpp
@@ -0,0 +1,50 @@
+#include "campus_widget.h"
+#include "campus.h"
+
+//Note that the constructor initializes a campus associated with the object so that we know which campus the user is messing with.
+CampusWidget::CampusWidget(Campus campusPassed, MainWindow *parent)
+    :campus{campusPassed}, parent{parent}
+{
+
+    //Creating all the UI elements dynamically for each campus column in the list widget.
+    layout = new QHBoxLayout(this);
+    stateLabel = new QLabel(campus.getState());
+    stateLabel->setFixedWidth(100);
+    nameLabel = new QLabel(campus.getStartCollege());
+    nameLabel->setFixedWidth(270);
+    endC = new QComboBox();
+//    endC->setFixedWidth(230);
+    menuItems = new QComboBox();
+
+    for (int i = 0; i < this->campus.getEndCollege().size(); i++)
+    {
+        endC->addItem(QString(this->campus.getEndCollege()[i] + " - " + QString::number(this->campus.getDistances()[i]) + " miles"));
+        //qInfo() << campus.getEndCollege()[i] << "\n";
+    }
+    //Iterating over the menu to add items to the menu ComboBox
+    for (int i = 0; i < this->campus.getMenu().size(); i++)
+    {
+        menuItems->addItem(QString(this->campus.getMenu()[i].name + ": $" + QString::number(this->campus.getMenu()[i].price)));
+    }
+
+
+
+    //Adding all input widgets to our QHBoxLayout that groups them all together.
+    layout->addWidget(stateLabel);
+    layout->addWidget(nameLabel);
+    layout->addWidget(endC);
+    layout->addWidget(menuItems);
+
+}
+
+
+
+
+
+CampusWidget::~CampusWidget(){
+    delete layout;
+    delete endC;
+    delete stateLabel;
+    delete nameLabel;
+    delete menuItems;
+}
---
+++ CS1D-Group-Project-2-main/CollegeTouring/fromsaddleback.cpp
@@ -0,0 +1,6 @@
+#include "fromsaddleback.h"
+
+fromsaddleback::fromsaddleback()
+{
+
+}
