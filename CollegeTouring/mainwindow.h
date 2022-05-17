ea366912dcac86a51982f71bf7eb6004ec6fedd4
42-Max-42 <91645022+42-Max-42@users.noreply.github.com>
2022-05-17 00:06:28 -0700

    Add files via upload

---
+++ CollegeTouring/mainwindow.h
@@ -0,0 +1,135 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <iostream>
+#include "campus.h"
+#include "database.h"
+#include "login.h"
+#include <QListWidget>
+#include "campus_widget.h"
+#include "map.h"
+#include "menuwidget.h"
+
+using namespace std;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void addCampus(Campus campus);
+    vector<QString> getMenuList() const { return menuList; }
+    void addMenuItem(Campus restaurant, Souvenir item);
+    void addItem(Campus campus, vector<Souvenir> newMenu);
+    void deleteItem(Campus campus, vector<Souvenir> newMenu);
+    void editItem(Campus campus, int index);
+    Database db;
+public slots:
+    void unlockTab();
+private slots:
+    void on_openCA_clicked();
+
+    void on_adminPop_clicked();
+
+    void Update();
+
+    void on_UnderSelected_clicked();
+
+    void on_UnderAll_clicked();
+
+    void on_UnderInitial_clicked();
+
+    void on_actionCustom_Trip_to_Any_triggered();
+
+    void on_tabWidget_currentChanged(int index);
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_CustomTripList_itemClicked(QListWidgetItem *item);
+
+    void on_AddToCart_clicked();
+
+    void on_CustomTripPrices_itemClicked(QListWidgetItem *item);
+
+    void on_CustomTripSouvs_itemClicked(QListWidgetItem *item);
+
+    void on_pushButton_3_clicked();
+
+    void on_openAll_clicked();
+
+    void on_sortStateEnd_clicked();
+
+    void on_sortStateStart_clicked();
+
+    void on_actionCustrom_Dijkstra_triggered();
+
+    void on_addStateD_clicked();
+
+    void on_CustomConvert_clicked();
+
+    void on_CustomConvert_2_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_addStateD_2_clicked();
+
+    //void on_pushButton_6_clicked();
+
+    void on_CustomConvert_3_clicked();
+
+    void on_AllSaddlebackList_itemClicked(QListWidgetItem *item);
+
+    void on_SaddlebackTripSouvs_itemClicked(QListWidgetItem *item);
+
+    void on_SaddlebackTripPrices_itemClicked(QListWidgetItem *item);
+
+    void on_AddToCartSA_clicked();
+
+    void on_SaddlbackFinish_clicked();
+
+    void on_actionAll_from_Saddleback_triggered();
+
+    void on_StartMich_clicked();
+
+    void on_CustomConvert_4_clicked();
+
+    void on_MichTripSouvs_itemClicked(QListWidgetItem *item);
+
+    void on_MichTripPrices_itemClicked(QListWidgetItem *item);
+
+    void on_AddToCartM_clicked();
+
+    void on_MichSpecificList_itemClicked(QListWidgetItem *item);
+
+    void on_MichFinish_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+//    Database db;
+
+    bool log;
+    Login* loginpopup;
+
+    Map collegeMap;
+    vector<Campus> campuses;
+    vector<QString> nameList;
+    vector<QString> menuList;
+    std::multimap<int, Souvenir> selectedItems;
+    std::vector<Campus> selectedCampus; /*!< Vector of selected campuses */
+
+
+};
+#endif // MAINWINDOW_H
---
+++ CollegeTouring/map.h
@@ -0,0 +1,96 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <vector>
+#include <QObject>
+#include<QDebug>
+#include "campus.h"
+
+#define TABLE_SIZE 400//entries plus 1
+
+#define PRIME 13
+
+struct HashStruct
+{
+    QString origin = "";
+    QString dest = "";
+    int num = -1;
+    double dist = 0;
+    bool select = false;
+    bool visited = false;
+};
+
+class Map
+{
+public:
+    Map()
+{
+    //this->TABLE_SIZE = size;
+    for(int i = 0; i < 150; i++)
+    {
+        hashTable[i].num = -1;
+        hashTable[i].origin = "";
+        hashTable[i].dest = "";
+    }
+};
+    int hash1(pair<int,double> key)
+{
+    return(key.first % TABLE_SIZE);
+};
+
+    int hash2(pair<int,double> key)
+{
+    return (PRIME - (key.first % PRIME));
+};
+
+    void insertHash(pair<int,double> key, QString origin, QString dest);
+
+    void deleteKey(pair<int,double> key);
+
+    void putVectorinHere(vector<Campus> campuses);
+
+    int getDist(QString origin, QString dest);
+
+    QString getOrigin(pair<int,double> key);
+
+    QString getDest(pair<int,double> key);
+
+    void selected(vector<QString> selec, int size);
+
+    void recurSelec(pair<int,double> key);
+
+    void dijkstra(pair<int,double> key);
+
+    void initPathDist(vector<QString> Sel);
+
+    void pathsInAns(int j);
+
+    void BFS(pair<int, double> key);
+
+    void DFS(int q);//Starts at UCI
+
+    void print();
+
+    void dijkstraNext(int pathIndex);
+
+    HashStruct hashTable[TABLE_SIZE];
+
+    vector<HashStruct> ans;
+
+    vector<pair<int,double>> pathDists;
+
+    vector<int> parent;
+
+    vector<int> numToPrint;
+
+    vector<double> dist;
+    int coll = 0; //number of colleges in map (used in dijkstra setup)
+private:
+
+
+    int s = 0; //number of selected items
+
+
+};
+
+#endif // MAP_H
