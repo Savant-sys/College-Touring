1db3c7b49e278ab2d0471cc1262638f55c59a143
EPaz0 <79540181+EPaz0@users.noreply.github.com>
2022-05-17 17:59:55 -0700

    Add files via upload

---
+++ CS1D-Group-Project-2-main/CS1D-Group-Project-2-main/build-CollegeTouring-Desktop_Qt_6_2_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,340 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../CollegeTouring/mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    const uint offsetsAndSize[104];
+    char stringdata0[1225];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 10), // "MainWindow"
+QT_MOC_LITERAL(11, 9), // "unlockTab"
+QT_MOC_LITERAL(21, 0), // ""
+QT_MOC_LITERAL(22, 28), // "getEndCollegesFromSaddleback"
+QT_MOC_LITERAL(51, 15), // "vector<QString>"
+QT_MOC_LITERAL(67, 26), // "getDistancesFromSaddleback"
+QT_MOC_LITERAL(94, 14), // "vector<double>"
+QT_MOC_LITERAL(109, 17), // "on_openCA_clicked"
+QT_MOC_LITERAL(127, 19), // "on_adminPop_clicked"
+QT_MOC_LITERAL(147, 6), // "Update"
+QT_MOC_LITERAL(154, 24), // "on_UnderSelected_clicked"
+QT_MOC_LITERAL(179, 19), // "on_UnderAll_clicked"
+QT_MOC_LITERAL(199, 23), // "on_UnderInitial_clicked"
+QT_MOC_LITERAL(223, 37), // "on_actionCustom_Trip_to_Any_t..."
+QT_MOC_LITERAL(261, 27), // "on_tabWidget_currentChanged"
+QT_MOC_LITERAL(289, 5), // "index"
+QT_MOC_LITERAL(295, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(317, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(341, 29), // "on_CustomTripList_itemClicked"
+QT_MOC_LITERAL(371, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(388, 4), // "item"
+QT_MOC_LITERAL(393, 20), // "on_AddToCart_clicked"
+QT_MOC_LITERAL(414, 31), // "on_CustomTripPrices_itemClicked"
+QT_MOC_LITERAL(446, 30), // "on_CustomTripSouvs_itemClicked"
+QT_MOC_LITERAL(477, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(501, 18), // "on_openAll_clicked"
+QT_MOC_LITERAL(520, 25), // "on_sortStateStart_clicked"
+QT_MOC_LITERAL(546, 35), // "on_actionCustrom_Dijkstra_tri..."
+QT_MOC_LITERAL(582, 20), // "on_addStateD_clicked"
+QT_MOC_LITERAL(603, 24), // "on_CustomConvert_clicked"
+QT_MOC_LITERAL(628, 26), // "on_CustomConvert_2_clicked"
+QT_MOC_LITERAL(655, 21), // "on_sortStates_clicked"
+QT_MOC_LITERAL(677, 20), // "on_sortStart_clicked"
+QT_MOC_LITERAL(698, 28), // "on_allFromSaddleback_clicked"
+QT_MOC_LITERAL(727, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(751, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(775, 22), // "on_addStateD_2_clicked"
+QT_MOC_LITERAL(798, 26), // "on_CustomConvert_3_clicked"
+QT_MOC_LITERAL(825, 32), // "on_AllSaddlebackList_itemClicked"
+QT_MOC_LITERAL(858, 34), // "on_SaddlebackTripSouvs_itemCl..."
+QT_MOC_LITERAL(893, 35), // "on_SaddlebackTripPrices_itemC..."
+QT_MOC_LITERAL(929, 22), // "on_AddToCartSA_clicked"
+QT_MOC_LITERAL(952, 26), // "on_SaddlbackFinish_clicked"
+QT_MOC_LITERAL(979, 38), // "on_actionAll_from_Saddleback_..."
+QT_MOC_LITERAL(1018, 20), // "on_StartMich_clicked"
+QT_MOC_LITERAL(1039, 26), // "on_CustomConvert_4_clicked"
+QT_MOC_LITERAL(1066, 28), // "on_MichTripSouvs_itemClicked"
+QT_MOC_LITERAL(1095, 29), // "on_MichTripPrices_itemClicked"
+QT_MOC_LITERAL(1125, 21), // "on_AddToCartM_clicked"
+QT_MOC_LITERAL(1147, 31), // "on_MichSpecificList_itemClicked"
+QT_MOC_LITERAL(1179, 21), // "on_MichFinish_clicked"
+QT_MOC_LITERAL(1201, 23) // "on_pushButton_6_clicked"
+
+    },
+    "MainWindow\0unlockTab\0\0"
+    "getEndCollegesFromSaddleback\0"
+    "vector<QString>\0getDistancesFromSaddleback\0"
+    "vector<double>\0on_openCA_clicked\0"
+    "on_adminPop_clicked\0Update\0"
+    "on_UnderSelected_clicked\0on_UnderAll_clicked\0"
+    "on_UnderInitial_clicked\0"
+    "on_actionCustom_Trip_to_Any_triggered\0"
+    "on_tabWidget_currentChanged\0index\0"
+    "on_pushButton_clicked\0on_pushButton_2_clicked\0"
+    "on_CustomTripList_itemClicked\0"
+    "QListWidgetItem*\0item\0on_AddToCart_clicked\0"
+    "on_CustomTripPrices_itemClicked\0"
+    "on_CustomTripSouvs_itemClicked\0"
+    "on_pushButton_3_clicked\0on_openAll_clicked\0"
+    "on_sortStateStart_clicked\0"
+    "on_actionCustrom_Dijkstra_triggered\0"
+    "on_addStateD_clicked\0on_CustomConvert_clicked\0"
+    "on_CustomConvert_2_clicked\0"
+    "on_sortStates_clicked\0on_sortStart_clicked\0"
+    "on_allFromSaddleback_clicked\0"
+    "on_pushButton_5_clicked\0on_pushButton_4_clicked\0"
+    "on_addStateD_2_clicked\0"
+    "on_CustomConvert_3_clicked\0"
+    "on_AllSaddlebackList_itemClicked\0"
+    "on_SaddlebackTripSouvs_itemClicked\0"
+    "on_SaddlebackTripPrices_itemClicked\0"
+    "on_AddToCartSA_clicked\0"
+    "on_SaddlbackFinish_clicked\0"
+    "on_actionAll_from_Saddleback_triggered\0"
+    "on_StartMich_clicked\0on_CustomConvert_4_clicked\0"
+    "on_MichTripSouvs_itemClicked\0"
+    "on_MichTripPrices_itemClicked\0"
+    "on_AddToCartM_clicked\0"
+    "on_MichSpecificList_itemClicked\0"
+    "on_MichFinish_clicked\0on_pushButton_6_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      45,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  284,    2, 0x0a,    1 /* Public */,
+       3,    0,  285,    2, 0x0a,    2 /* Public */,
+       5,    0,  286,    2, 0x0a,    3 /* Public */,
+       7,    0,  287,    2, 0x08,    4 /* Private */,
+       8,    0,  288,    2, 0x08,    5 /* Private */,
+       9,    0,  289,    2, 0x08,    6 /* Private */,
+      10,    0,  290,    2, 0x08,    7 /* Private */,
+      11,    0,  291,    2, 0x08,    8 /* Private */,
+      12,    0,  292,    2, 0x08,    9 /* Private */,
+      13,    0,  293,    2, 0x08,   10 /* Private */,
+      14,    1,  294,    2, 0x08,   11 /* Private */,
+      16,    0,  297,    2, 0x08,   13 /* Private */,
+      17,    0,  298,    2, 0x08,   14 /* Private */,
+      18,    1,  299,    2, 0x08,   15 /* Private */,
+      21,    0,  302,    2, 0x08,   17 /* Private */,
+      22,    1,  303,    2, 0x08,   18 /* Private */,
+      23,    1,  306,    2, 0x08,   20 /* Private */,
+      24,    0,  309,    2, 0x08,   22 /* Private */,
+      25,    0,  310,    2, 0x08,   23 /* Private */,
+      26,    0,  311,    2, 0x08,   24 /* Private */,
+      27,    0,  312,    2, 0x08,   25 /* Private */,
+      28,    0,  313,    2, 0x08,   26 /* Private */,
+      29,    0,  314,    2, 0x08,   27 /* Private */,
+      30,    0,  315,    2, 0x08,   28 /* Private */,
+      31,    0,  316,    2, 0x08,   29 /* Private */,
+      32,    0,  317,    2, 0x08,   30 /* Private */,
+      33,    0,  318,    2, 0x08,   31 /* Private */,
+      34,    0,  319,    2, 0x08,   32 /* Private */,
+      35,    0,  320,    2, 0x08,   33 /* Private */,
+      36,    0,  321,    2, 0x08,   34 /* Private */,
+      37,    0,  322,    2, 0x08,   35 /* Private */,
+      38,    1,  323,    2, 0x08,   36 /* Private */,
+      39,    1,  326,    2, 0x08,   38 /* Private */,
+      40,    1,  329,    2, 0x08,   40 /* Private */,
+      41,    0,  332,    2, 0x08,   42 /* Private */,
+      42,    0,  333,    2, 0x08,   43 /* Private */,
+      43,    0,  334,    2, 0x08,   44 /* Private */,
+      44,    0,  335,    2, 0x08,   45 /* Private */,
+      45,    0,  336,    2, 0x08,   46 /* Private */,
+      46,    1,  337,    2, 0x08,   47 /* Private */,
+      47,    1,  340,    2, 0x08,   49 /* Private */,
+      48,    0,  343,    2, 0x08,   51 /* Private */,
+      49,    1,  344,    2, 0x08,   52 /* Private */,
+      50,    0,  347,    2, 0x08,   54 /* Private */,
+      51,    0,  348,    2, 0x08,   55 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    0x80000000 | 4,
+    0x80000000 | 6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   15,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->unlockTab(); break;
+        case 1: { vector<QString> _r = _t->getEndCollegesFromSaddleback();
+            if (_a[0]) *reinterpret_cast< vector<QString>*>(_a[0]) = std::move(_r); }  break;
+        case 2: { vector<double> _r = _t->getDistancesFromSaddleback();
+            if (_a[0]) *reinterpret_cast< vector<double>*>(_a[0]) = std::move(_r); }  break;
+        case 3: _t->on_openCA_clicked(); break;
+        case 4: _t->on_adminPop_clicked(); break;
+        case 5: _t->Update(); break;
+        case 6: _t->on_UnderSelected_clicked(); break;
+        case 7: _t->on_UnderAll_clicked(); break;
+        case 8: _t->on_UnderInitial_clicked(); break;
+        case 9: _t->on_actionCustom_Trip_to_Any_triggered(); break;
+        case 10: _t->on_tabWidget_currentChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->on_pushButton_clicked(); break;
+        case 12: _t->on_pushButton_2_clicked(); break;
+        case 13: _t->on_CustomTripList_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 14: _t->on_AddToCart_clicked(); break;
+        case 15: _t->on_CustomTripPrices_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 16: _t->on_CustomTripSouvs_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 17: _t->on_pushButton_3_clicked(); break;
+        case 18: _t->on_openAll_clicked(); break;
+        case 19: _t->on_sortStateStart_clicked(); break;
+        case 20: _t->on_actionCustrom_Dijkstra_triggered(); break;
+        case 21: _t->on_addStateD_clicked(); break;
+        case 22: _t->on_CustomConvert_clicked(); break;
+        case 23: _t->on_CustomConvert_2_clicked(); break;
+        case 24: _t->on_sortStates_clicked(); break;
+        case 25: _t->on_sortStart_clicked(); break;
+        case 26: _t->on_allFromSaddleback_clicked(); break;
+        case 27: _t->on_pushButton_5_clicked(); break;
+        case 28: _t->on_pushButton_4_clicked(); break;
+        case 29: _t->on_addStateD_2_clicked(); break;
+        case 30: _t->on_CustomConvert_3_clicked(); break;
+        case 31: _t->on_AllSaddlebackList_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 32: _t->on_SaddlebackTripSouvs_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 33: _t->on_SaddlebackTripPrices_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 34: _t->on_AddToCartSA_clicked(); break;
+        case 35: _t->on_SaddlbackFinish_clicked(); break;
+        case 36: _t->on_actionAll_from_Saddleback_triggered(); break;
+        case 37: _t->on_StartMich_clicked(); break;
+        case 38: _t->on_CustomConvert_4_clicked(); break;
+        case 39: _t->on_MichTripSouvs_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 40: _t->on_MichTripPrices_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 41: _t->on_AddToCartM_clicked(); break;
+        case 42: _t->on_MichSpecificList_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 43: _t->on_MichFinish_clicked(); break;
+        case 44: _t->on_pushButton_6_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSize,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t
+, QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<vector<QString>, std::false_type>, QtPrivate::TypeAndForceComplete<vector<double>, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<int, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 45)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 45;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 45)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 45;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ CS1D-Group-Project-2-main/CS1D-Group-Project-2-main/build-CollegeTouring-Desktop_Qt_6_2_2_MinGW_64_bit-Debug/ui_allfromsaddleback.h
@@ -0,0 +1,77 @@
+/********************************************************************************
+** Form generated from reading UI file 'allfromsaddleback.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ALLFROMSADDLEBACK_H
+#define UI_ALLFROMSADDLEBACK_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QTableWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Allfromsaddleback
+{
+public:
+    QGridLayout *gridLayout;
+    QTableWidget *saddlebackTable;
+
+    void setupUi(QDialog *Allfromsaddleback)
+    {
+        if (Allfromsaddleback->objectName().isEmpty())
+            Allfromsaddleback->setObjectName(QString::fromUtf8("Allfromsaddleback"));
+        Allfromsaddleback->resize(543, 418);
+        gridLayout = new QGridLayout(Allfromsaddleback);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        saddlebackTable = new QTableWidget(Allfromsaddleback);
+        if (saddlebackTable->columnCount() < 3)
+            saddlebackTable->setColumnCount(3);
+        QFont font;
+        font.setBold(true);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        __qtablewidgetitem->setFont(font);
+        saddlebackTable->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        __qtablewidgetitem1->setFont(font);
+        saddlebackTable->setHorizontalHeaderItem(1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        __qtablewidgetitem2->setFont(font);
+        saddlebackTable->setHorizontalHeaderItem(2, __qtablewidgetitem2);
+        saddlebackTable->setObjectName(QString::fromUtf8("saddlebackTable"));
+
+        gridLayout->addWidget(saddlebackTable, 0, 0, 1, 1);
+
+
+        retranslateUi(Allfromsaddleback);
+
+        QMetaObject::connectSlotsByName(Allfromsaddleback);
+    } // setupUi
+
+    void retranslateUi(QDialog *Allfromsaddleback)
+    {
+        Allfromsaddleback->setWindowTitle(QCoreApplication::translate("Allfromsaddleback", "Dialog", nullptr));
+        QTableWidgetItem *___qtablewidgetitem = saddlebackTable->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QCoreApplication::translate("Allfromsaddleback", "From Starting College", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = saddlebackTable->horizontalHeaderItem(1);
+        ___qtablewidgetitem1->setText(QCoreApplication::translate("Allfromsaddleback", "To Ending Colleges", nullptr));
+        QTableWidgetItem *___qtablewidgetitem2 = saddlebackTable->horizontalHeaderItem(2);
+        ___qtablewidgetitem2->setText(QCoreApplication::translate("Allfromsaddleback", "Distances", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Allfromsaddleback: public Ui_Allfromsaddleback {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ALLFROMSADDLEBACK_H
